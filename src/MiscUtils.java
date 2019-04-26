8a11f3df9d6ea4a42c3cb945a776677518f46313
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2019-04-26 01:25:29 -0500

    Add a few more utilities.

--- src/MiscUtils.java
+++ src/MiscUtils.java
@@ -1,11 +1,49 @@
-import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Random;
 import java.util.function.Function;
 
 /**
  * Some utilities for working with iterators.
  */
 public class MiscUtils {
+  public static Random rand = new Random();
+  
+  /**
+   * Randomly permute an array.
+   */
+  public static <T> void randomlyPermute(T[] values) {
+    for (int i = 0; i < values.length; i++) {
+      MiscUtils.swap(values, i, rand.nextInt(values.length));
+    } // for
+  } // randomlyPermute(T[])
+  
+  /**
+   * Randomly permute an array of integers
+   */
+  public static void randomlyPermute(int[] values) {
+    for (int i = 0; i < values.length; i++) {
+      MiscUtils.swap(values,  i,  rand.nextInt(values.length));
+    } // for
+  } // randomlyPermute
+  
+  /**
+   * Swap two objects in an array.
+   */
+  public static <T> void swap(T[] values, int i, int j) {
+    T temp = values[i];
+    values[i] = values[j];
+    values[j] = temp;
+  } // swap
+
+  /**
+   * Swap two ints in an array.
+   */
+  public static void swap(int[] values, int i, int j) {
+    int temp = values[i];
+    values[i] = values[j];
+    values[j] = temp;
+  } // swap
+
   /**
    * Make a new iterator that works just like the given iterator, except that
    * the given function is applied to the result of next.
